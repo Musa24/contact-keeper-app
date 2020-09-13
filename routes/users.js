fd0e1c7d20dd8143f1c3cee28d35cce84dbff184
MUSA SALUMU MUHOZYA <musa24@MacBook-Pro-MUSA.local>
2020-09-13 14:57:43 +0300

    Create & Respond with JSON Web Token

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 //Express validator
 const { body, validationResult } = require('express-validator');
@@ -21,12 +24,49 @@ router.post(
       min: 6,
     }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
-    } else {
-      res.send('All fine');
+    }
+
+    const { name, email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (user) {
+        return res.status(400).json({ msg: 'User already exists' });
+      }
+
+      user = new User({
+        name,
+        email,
+        password,
+      });
+
+      //encrypting the password
+
+      const salt = await bcrypt.genSaltSync(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+      //Create and Send token to the user
+      const payLoad = {
+        user: {
+          id: user.id,
+        },
+      };
+      jwt.sign(
+        payLoad,
+        config.get('jwtScret'),
+        { expiresIn: 3600000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send('Server Error');
     }
   }
 );
