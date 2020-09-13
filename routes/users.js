2ee38033a0ad1e4a439ca1ea12ca33de13174386
MUSA SALUMU MUHOZYA <musa24@MacBook-Pro-MUSA.local>
2020-09-13 03:51:00 +0300

    User Model and Validation

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,34 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User');
+
+//Express validator
+const { body, validationResult } = require('express-validator');
 
 //@route           POST      api/user
 // @desc           Register a User
 // @Access         Public
-router.post('/', (req, res) => {
-  res.send('Register a User');
-});
+router.post(
+  '/',
+  [
+    //Name must not be empty
+    body('name', 'Please add a name').not().isEmpty(),
+    //Valid email
+
+    body('email', 'Please include a valid email').isEmail(),
+    //Passoord must be at least 6 characters
+    body('password', 'Please enter a passowrd with 6 characters').isLength({
+      min: 6,
+    }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    } else {
+      res.send('All fine');
+    }
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+//connect Database
+connectDB();
 
 app.get('/', (req, res) => {
   //   res.send('Hello world');
   res.json({ name: 'Musa' });
 });
+
+//Middleware for helping to use req.body
+// app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ extended: false })); //Accept JSON DATA
 
 //DEFINE ALL THE ROUTER
 app.use('/api/users', require('./routes/users'));
