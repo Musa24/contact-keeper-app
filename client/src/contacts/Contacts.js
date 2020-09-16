96a3d77845a4261a72c90189d456f1a1850f5eb3
MUSA SALUMU MUHOZYA <musa24@MacBook-Pro-MUSA.local>
2020-09-16 16:39:59 +0300

    Animation with React-transition-group

--- client/src/contacts/Contacts.js
+++ client/src/contacts/Contacts.js
@@ -11,13 +11,19 @@ function Contacts() {
   }
   return (
     <Fragment>
-      {filtered !== null
-        ? filtered.map((contact) => (
-            <ContactItem key={contact.id} contact={contact} />
-          ))
-        : contacts.map((contact) => (
-            <ContactItem key={contact.id} contact={contact} />
-          ))}
+      <TransitionGroup>
+        {filtered !== null
+          ? filtered.map((contact) => (
+              <CSSTransition key={contact.id} timeout={500} classNames="item">
+                <ContactItem contact={contact} />
+              </CSSTransition>
+            ))
+          : contacts.map((contact) => (
+              <CSSTransition key={contact.id} timeout={500} classNames="item">
+                <ContactItem contact={contact} />
+              </CSSTransition>
+            ))}
+      </TransitionGroup>
     </Fragment>
   );
 }
